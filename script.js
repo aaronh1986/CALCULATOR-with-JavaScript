e9d39391d06409aa0c3ecc654a0b97fbc1a85143
aaronh1986 <aaronharbourne@gmail.com>
2021-05-03 20:31:04 +0000

    Ready to go

---
+++ script.js
@@ -0,0 +1,9 @@
+let heading = document.createElement('h1');
+heading.innerText = "Hello it worked";
+document.body.appendChild(heading);
+
+
+
+var btn = document.createElement("BUTTON");  
+btn.innerHTML = "CLICK ME";        
+document.body.appendChild(btn);     
