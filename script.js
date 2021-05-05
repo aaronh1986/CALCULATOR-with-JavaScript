8bdfa58d2fc93aad8bd4fe51758fc1a163da7daa
aaronh1986 <aaronharbourne@gmail.com>
2021-05-05 12:59:55 +0000

    Add calculator html

--- script.js
+++
@@ -1,9 +0,0 @@
-let heading = document.createElement('h1');
-heading.innerText = "Hello it worked";
-document.body.appendChild(heading);
-
-
-
-var btn = document.createElement("BUTTON");  
-btn.innerHTML = "CLICK ME";        
-document.body.appendChild(btn);     
